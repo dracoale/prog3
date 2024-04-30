b1663b47a58d880e971d2e90f169f122ecdda61d
dracoale <121072013+dracoale@users.noreply.github.com>
2024-04-29 21:03:37 -0500

    Add files via upload

---
+++ softUsuarioModel/src/pe/edu/pucp/tienda/usuario/model/EstadoCuenta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.tienda.usuario.model;
+
+/**
+ *
+ * @author james
+ */
+public enum EstadoCuenta{
+    ACTIVO, DESACTIVADO, SUSPENDIDO
+}
