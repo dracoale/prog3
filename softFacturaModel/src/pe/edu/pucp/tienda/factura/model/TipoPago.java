8b12cf8268bcf655a07dbd433d5b751faaac0c2d
dracoale <121072013+dracoale@users.noreply.github.com>
2024-04-29 21:14:19 -0500

    Add files via upload

---
+++ softFacturaModel/src/pe/edu/pucp/tienda/factura/model/TipoPago.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package pe.edu.pucp.tienda.factura.model;
+
+/**
+ *
+ * @author USER
+ */
+public enum TipoPago {
+    VISA,PAYPAL,CUPON
+}
