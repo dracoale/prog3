2c5667d2ed0aa4157fa7a4b1cefc89a12db59e44
darklune1929 <59780274+darklune1929@users.noreply.github.com>
2024-05-27 16:13:04 -0500

    Arreglando el main

--- softPedidoController/softFacturaModel/src/pe/edu/pucp/tienda/factura/model/EstadoFactura.java
+++ softPedidoController/softFacturaModel/src/pe/edu/pucp/tienda/factura/model/EstadoFactura.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,14 +7,3 @@ package pe.edu.pucp.tienda.factura.model;
 public enum EstadoFactura {
     ACTIVO, DESACTIVADO
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package pe.edu.pucp.tienda.factura.model;
-
-public enum EstadoFactura {
-    ACTIVO, DESACTIVADO
-}
->>>>>>> 4bfb077fc66817bc69a0a458d3156109c3e875f5
--- softPedidoController/softFacturaModel/src/pe/edu/pucp/tienda/factura/model/TipoPago.java
+++ softPedidoController/softFacturaModel/src/pe/edu/pucp/tienda/factura/model/TipoPago.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
@@ -12,18 +11,3 @@ package pe.edu.pucp.tienda.factura.model;
 public enum TipoPago {
     VISA,PAYPAL,CUPON
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
-package pe.edu.pucp.tienda.factura.model;
-
-/**
- *
- * @author USER
- */
-public enum TipoPago {
-    VISA,PAYPAL,CUPON
-}
->>>>>>> 4bfb077fc66817bc69a0a458d3156109c3e875f5
