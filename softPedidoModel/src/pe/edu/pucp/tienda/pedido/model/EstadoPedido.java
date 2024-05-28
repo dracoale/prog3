39f2f5b64ea5c7b158b86c1cf65f28aa4cfbb08a
Paul C <paul.caceres@pucp.edu.pe>
2024-05-28 10:59:29 -0500

    d

--- softPedidoModel/src/pe/edu/pucp/tienda/pedido/model/EstadoPedido.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
-package pe.edu.pucp.tienda.pedido.model;
-
-public enum EstadoPedido {
-    ENTREGADA,PROCESADA,CANCELADA,EN_CAMINO
-}
